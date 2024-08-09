cd3aa09aa8d84fe20a817ddfbe1fe2eddc45d5e5
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-08-08 23:19:35 -0300

    control -sonido

--- Services/ReproduccionMusica.cs
+++ Services/ReproduccionMusica.cs
@@ -2,30 +2,34 @@ using NAudio.Wave;
 
 public static class ReproduccionMusica
 {
-    private static WaveOutEvent? waveOut;
-    private static AudioFileReader? audioFileReader;
+    private static WaveOutEvent? _waveOut;
+    private static AudioFileReader? _audioFileReader;
 
     public static void ReproducirMusica(string rutaArchivo)
     {
-        DetenerMusica(); // control 
-        audioFileReader = new AudioFileReader(rutaArchivo);
-        waveOut.Init(audioFileReader);
-        waveOut.Play(); // Reproduce el audio
+        DetenerMusica(); // 
+        _audioFileReader = new AudioFileReader(rutaArchivo);
+        _waveOut = new WaveOutEvent
+        {
+            Volume = 0.5f // Ajusta el volumen si es necesario
+        };
+        _waveOut.Init(_audioFileReader);
+        _waveOut.Play(); // Reproduce el audio
     }
 
     public static void DetenerMusica()
     {
-        if (waveOut != null)
+        if (_waveOut != null)
         {
-            waveOut.Stop();
-            waveOut.Dispose();
-            waveOut = null;
+            _waveOut.Stop();
+            _waveOut.Dispose();
+            _waveOut = null;
         }
 
-        if (audioFileReader != null)
+        if (_audioFileReader != null)
         {
-            audioFileReader.Dispose();
-            audioFileReader = null;
+            _audioFileReader.Dispose();
+            _audioFileReader = null;
         }
     }
 }
